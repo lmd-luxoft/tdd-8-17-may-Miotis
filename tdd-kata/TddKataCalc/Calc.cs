bf4544ab32a1e6b264df45faadd3d5ed524aa029
Maksym Remnov <maksym.remnov2@halliburton.com>
2023-05-10 10:55:35 +0300

    Initial commit with all bins

---
+++ tdd-kata/TddKataCalc/Calc.cs
@@ -0,0 +1,10 @@
+﻿namespace TddKataCalc
+{
+    public class Calc
+    {
+        public int Add(string digits=default)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ tdd-kata/TddKataCalc/CalcException.cs
@@ -0,0 +1,5 @@
+﻿namespace TddKataCalc;
+
+public class CalcException : Exception
+{
+}
